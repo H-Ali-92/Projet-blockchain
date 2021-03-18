d23b50462c2896dd0b5cc590ed064fd33d0be6f5
H-Ali-92 <80901958+H-Ali-92@users.noreply.github.com>
2021-03-18 17:31:50 +0100

    Update Projet Blockchain - code.py

--- Projet Blockchain - code.py
+++ Projet Blockchain - code.py
@@ -32,15 +32,18 @@ class Block(object):
             self.nonce += 1
             self.hash = calculateHash(self)
 
-#Ma classe Blockchain me permet d'inplémenter la difficulté, de créer le bloc de génèse (le tout premier bloc de ma chaîne) afin de faire fonctionner correctement ma blockchain, de définir comment créer les nouveaux blocs de la chaîne, et enfin de vérifier si ma blockchain est valide
+#Ma classe Blockchain qui va me permettre de créer le bloc de génèse (le tout premier bloc de ma chaîne) afin de faire fonctionner correctement ma blockchain, de définir comment créer les nouveaux blocs de la chaîne, et enfin de vérifier si ma blockchain est valide
 class Blockchain(object):
+    #Ma blockchain est une liste de blocs, et mon constructeur prend comme unique argument la “difficulté”
     def __init__(self, difficulty):
         self.difficulty = difficulty
         self.blocks = []
 
-        #Je défini mon bloc de génèse selon les caractéristiques suivantes : un index de "0", un préviousHash inexistant, la date et l'heure actuelle, et les données que j'ai laissé comme tel
+        #Je défini mon bloc de génèse selon les caractéristiques suivantes : un index de "0", un préviousHash nul, la date et l'heure actuelle, et les données que j'ai arbitrairement laissé comme tel
         genesisBlock = Block(0, None, datetime.now(), "genesis block", "origine", "traitements", "transport", "prix")
+        #A partir de ce bloc Genesis, je mine un bloc valide qui lui sera le premier de ma chaine
         genesisBlock.mineBlock(self.difficulty)
+        #Puis j’ajoute le bloc résultant comme premier bloc de ma blockchain
         self.blocks.append(genesisBlock)
 
     #La fonction newBlock (qui prend en entrée toutes le données que je veux mettre) permet de définir automatiquement l'index, le previousHash et la date et l'heure de chaque nouveau bloc
@@ -48,12 +51,12 @@ class Blockchain(object):
         latestBlock = self.blocks[-1]
         return(Block(latestBlock.index + 1, latestBlock.hash, datetime.now(), produit, origine, traitements, transport, prix))
 
-
+    #Il est nécessaire de miner ce bloc en fonction des données du bloc précédent afin de lui donner un hash valide et donc une intégrité vis-à-vis du reste de la blockchain, ce qui m’amène à la méthode addBlock. Je mine mon bloc et l’ajoute à la suite de la liste qui représente ma blockchain
     def addBlock(self, block):
         block.mineBlock(self.difficulty)
         self.blocks.append(block)
 
-    #La fonction isFirstBlockValid permet de savoir si mon bloc de génèse est valide, c-à-d qu'il doit respecter certaines conditions avant de pouvoir l'être. Ces conditions sont : l'index du bloc doit être égal à 0, son previousHash ne doit pas être inexistant et son hash ne doit ni être inexistant ni différent du hash qui lui a été attribué.
+    #La fonction isFirstBlockValid permet de savoir si mon bloc de génèse est valide, c-à-d qu'il doit respecter certaines conditions avant de pouvoir l'être. Ces conditions sont : l'index du bloc doit être égal à 0, son previousHash doit être nul et son hash ne doit ni être nul ni différent du hash qui lui a été attribué.
     #Si toutes ces conditions sont vérifiées, le premier bloc est donc valide
     def isFirstBlockValid(self):
         firstBlock = self.blocks[0]
@@ -69,7 +72,7 @@ class Blockchain(object):
 
         return True
 
-    #La fonction isValidBlock permet de savoir si chaque bloc après le genesisBlock est valide. Comme avec le genesisBlock, les autres blocs doivent respecter certaines condition afin de d'être valide. Ces conditions sont : l'index du bloc n doit être égal à l'index du bloc précédent+1, son previousHash doit être le Hash du bloc précédent et son hash ne doit ni être inexistant ni différent du hash qui lui a été attribué
+    #La fonction isValidBlock permet de savoir si chaque bloc après le genesisBlock est valide. Comme avec le genesisBlock, les autres blocs doivent respecter certaines condition afin d'être valide. Ces conditions sont : l'index du bloc n doit être égal à l'index du bloc précédent+1, son previousHash doit être le Hash du bloc précédent et son hash ne doit ni être nul ni différent du hash qui lui a été attribué
     def isValidBlock(self, block, previousBlock):
         if previousBlock.index+1 != block.index:
             return False
@@ -81,5 +84,5 @@ class Blockchain(object):
             return False
 
         return True
-
-#Work in progress...
+    
+#Work in progress
